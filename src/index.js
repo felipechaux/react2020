6c843790b0358a15752fde79c176a9518c0caeee
felipe <felchax@gmail.com>
2020-05-09 17:47:31 -0500

    update index, now json and package

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-ReactDOM.render(<h1>Seguimos aprendiendo - react avanzado</h1>,
+ReactDOM.render(<h1>Seguimos - react avanzado</h1>,
   document.getElementById('app'))
